690f4df45e4655f5029c4cf9d9205e028f20e7ea
Jianing <samosatar@gmail.com>
2025-01-01 23:07:43 +0800

    Init repo.

---
+++ algorithms.py
@@ -0,0 +1,64 @@
+import cv2
+from mtcnn import MTCNN
+import numpy as np
+
+from typing import Optional, Tuple
+
+
+def detect_face_eyes_mtcnn(image: np.ndarray) -> Tuple[Optional[np.ndarray], int]:
+    """
+    该函数使用 MTCNN 算法检测人脸和眼睛，并根据眼睛位置裁剪图像。
+
+    参数:
+    image (np.ndarray): 输入的图像数据，以 numpy 数组形式表示。
+
+    返回:
+    Tuple[Optional[np.ndarray], int]: 一个元组，第一个元素是裁剪后的图像，如果未检测到人脸或眼睛则为 None；
+                                第二个元素是状态码，0 表示成功，-1 表示未检测到人脸或眼睛。
+    """
+    # 初始化 MTCNN 检测器
+    detector = MTCNN()
+    # 使用 MTCNN 检测人脸
+    result = detector.detect_faces(image)
+    if result:
+        for face in result:
+            # 获取人脸关键点信息
+            keypoints = face['keypoints']
+            # 获取左眼和右眼的关键点
+            left_eye = keypoints['left_eye']
+            right_eye = keypoints['right_eye']
+
+            # cv2.circle(image, left_eye, 2, (0, 255, 0), 2)
+            # cv2.circle(image, right_eye, 2, (0, 255, 0), 2)
+
+            # 计算左右眼的中点
+            mid_point = ((left_eye[0] + right_eye[0])//2,
+                         (left_eye[1] + right_eye[1])//2)
+            # 计算左右眼之间的距离（曼哈顿距离）
+            distance = abs(left_eye[1] - right_eye[1]) + \
+                abs(left_eye[0] - right_eye[0])
+
+            # 计算裁剪区域的边界
+            x1, x2 = mid_point[0] - distance, mid_point[0] + distance
+            y1, y2 = mid_point[1] - distance//2, mid_point[1] + distance//2
+
+            # 裁剪图像
+            croped = image[y1:y2, x1:x2]
+            return croped, 0
+
+    return None, -1
+
+
+if __name__ == "__main__":
+    # 调用函数
+
+    image = cv2.imread('test.jpg')
+    croped, _ = detect_face_eyes_mtcnn(image)
+
+    result = cv2.resize(croped, (200, 100))
+
+    cv2.imshow('Image', result)
+    # 等待用户按键，0 表示无限等待，直到用户按下按键
+    cv2.waitKey(0)
+    # 销毁所有创建的窗口
+    cv2.destroyAllWindows()
---
+++ app.py
@@ -0,0 +1,67 @@
+from flask_cors import CORS
+from flask import Flask, request, send_file, send_from_directory
+from werkzeug.utils import secure_filename
+import os
+import cv2
+import numpy as np
+from algorithms import detect_face_eyes_mtcnn
+
+
+app = Flask(__name__)
+cors = CORS(app)
+
+
+def process_image(image_path, target_size=(200, 100)):
+    app.logger.info(image_path)
+    app.logger.info(type(image_path))
+    image = cv2.imread(image_path)
+    croped, flag = detect_face_eyes_mtcnn(image)
+
+    if flag >= 0:
+        # 缩放图片到目标大小
+        result = cv2.resize(croped, target_size)
+        return result
+    else:
+        # 缩放原始图片到目标大小
+        image = cv2.resize(image, target_size)
+        return image
+
+
+def combine_images(images):
+    # 简单的拼接，假设所有图像大小相同
+    rows = []
+    for i in range(0, len(images), 3):
+        row = np.hstack(images[i:i+3])
+        rows.append(row)
+    combined_image = np.vstack(rows)
+    return combined_image
+
+
+@app.route('/')
+def index():
+    return send_from_directory(app.static_folder, 'index.html')
+
+
+@app.route('/process_images', methods=['POST'])
+def process_images():
+    uploaded_files = request.files
+    processed_images = []
+    for _, file in enumerate(uploaded_files.values()):
+        filename = secure_filename(file.filename)
+        file_path = os.path.join('uploads', filename)
+        # file_path = 'uploads/' + filename
+        file.save(file_path)
+        processed_image = process_image(file_path)
+        processed_images.append(processed_image)
+        os.remove(file_path)
+    combined_image = combine_images(processed_images)
+    cv2.imwrite(os.path.join('outputs', 'processed_image.jpg'), combined_image)
+    return send_file('processed_image.jpg', mimetype='image/jpeg')
+
+
+if __name__ == "__main__":
+    if not os.path.exists('uploads'):
+        os.makedirs('uploads')
+    if not os.path.exists('outputs'):
+        os.makedirs('outputs')
+    app.run(debug=True)
